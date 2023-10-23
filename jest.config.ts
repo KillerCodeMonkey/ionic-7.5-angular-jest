f7e3cdf71909290152e31fde2e61bde3c18419e7
Bengt Weiße <bengtler@gmail.com>
2023-10-23 17:49:21 +0200

    fixed: transformIgnorePattern

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  transformIgnorePatterns: ['node_modules/(?!(.*\\.mjs)$)'],
+  transformIgnorePatterns: ['node_modules/(?!(@ionic/angular|@ionic/core|ionicons|@stencil/core|@angular/*)/)'],
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
   testEnvironment: 'jsdom'
